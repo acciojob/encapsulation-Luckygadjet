a0757ac7ba45a2ea3630a629a1d662a0fec83b74
Your Name <sabavathnaveen951@gmail.com>
2023-02-09 17:34:54 +0530

    First attempt of Encapsulation

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,10 +3,7 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    public RWOnly()
-    {
-        this.name="";
-    }
+
     public void setName(String name)
     {
         this.name = name;
