f4fcabebd118cd29d1ababd85e48bd9fa81afc81
Your Name <sabavathnaveen951@gmail.com>
2023-02-09 17:34:03 +0530

    First Attmept of Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        r.setName("Naveen");
+        r.getName();
+        // name has private access in com.driver.RWOnly
+
+    }
+
+
+
 }
